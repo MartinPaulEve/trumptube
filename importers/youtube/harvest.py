7fd66120ad4c5da294f3f36747974abae5e85d1c
Martin Eve <martin@martineve.com>
2016-12-14 11:53:37 -0500

    Dump outputs into a JSON file and skip those that already exist

--- importers/youtube/harvest.py
+++ importers/youtube/harvest.py
@@ -1,53 +1,72 @@
 from googleapiclient.discovery import build
-from googleapiclient.discovery import build_from_document
+import json
+import os.path
 
 
 class Harvester():
     def __init__(self):
+        """
+        Setup and build a Youtube query object.
+        Your youtube API token should go in credentials.txt in the importers/youtube directory
+        """
         with open("credentials.txt", "r") as creds:
             self.api_key = creds.read()
 
         self.service = build('youtube', 'v3', developerKey=self.api_key)
 
-    def get_comment_threads(self):
-        with open("sources.txt", "r") as videos:
-            sources = videos.readlines()
+    def get_comment_threads(self, video_id):
+        """
+        Fetches top-level comments for a video ID
+        :param video_id: The ID of the video
+        :return: The dictionary of comments
+        """
 
-            for source in sources:
-                video_id = source
+        results = self.service.commentThreads().list(
+            part="snippet",
+            videoId=video_id,
+            textFormat="plainText"
+        ).execute()
 
-                results = self.service.commentThreads().list(
-                    part="snippet",
-                    videoId=video_id,
-                    textFormat="plainText"
-                ).execute()
+        return results["items"]
 
-                for item in results["items"]:
-                    comment = item["snippet"]["topLevelComment"]
-                    author = comment["snippet"]["authorDisplayName"]
-                    text = comment["snippet"]["textDisplay"]
-                    print("Comment by %s: %s" % (author, text))
-
-                return results["items"]
-
-    # Call the API's comments.list method to list the existing comment replies.
     def get_comments(self, parent_id):
+        """
+        Fetches a thread of comment replies
+        :param parent_id: The parent comment ID
+        :return: The dictionary of reply comments
+        """
         results = self.service.comments().list(
             part="snippet",
             parentId=parent_id,
             textFormat="plainText"
         ).execute()
 
-        for item in results["items"]:
-            author = item["snippet"]["authorDisplayName"]
-            text = item["snippet"]["textDisplay"]
-            print("Comment by %s: %s" % (author, text))
-
         return results["items"]
 
 
 if __name__ == "__main__":
     harvester = Harvester()
-    results = harvester.get_comment_threads()
-    for res in results:
-        harvester.get_comments(res['id'])
+
+    with open("sources.txt", "r") as videos:
+
+        sources = videos.readlines()
+
+        for source in sources:
+            video_id = source
+
+            fname = "output/{0}.json".format(video_id)
+
+            if os.path.isfile(fname):
+               print("Skipping {0} as it already exists.".format(fname))
+            else:
+                print("Harvesting into {0}.".format(fname))
+                results = harvester.get_comment_threads(video_id)
+
+                json_out = {}
+                json_out['thread'] = results
+
+                for res in results:
+                    json_out[res['id']] = harvester.get_comments(res['id'])
+
+                with open(fname, "w") as json_writer:
+                    json_writer.write(json.dumps(json_out, ensure_ascii=False))
